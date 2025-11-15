7288a38440d246ac4907b92e8a52f396119165f7
ojsant <ojsant@utu.fi>
2025-11-15 21:32:01 +0200

    Fixes, now only middle day is considered for onset detection

--- wind_event_finder.py
+++ wind_event_finder.py
@@ -7,59 +7,93 @@ from scipy.ndimage import gaussian_filter1d
 from pyonset import Onset, BootstrapWindow
 from anisotropy import run_SEPevent
 
+import sys
+import logging
+filehandler = logging.FileHandler(filename="wind_events.log", encoding="utf-8")
+streamhandler = logging.StreamHandler(sys.stdout)
+logging.basicConfig(level=logging.INFO, format='%(asctime)s %(message)s', 
+                    datefmt='%m/%d/%Y %H:%M:%S', handlers=[filehandler, streamhandler], force=True)
+
+
+def check_for_gaps(data, start, end, max_gap=1):
+    flux_finite = data.dropna()
+    for i in range(len(flux_finite[start:end]) - 1):
+        if flux_finite.index[i + 1] - flux_finite.index[i] > pd.Timedelta(hours=max_gap):
+            return True
+    return False
 
 data_path = f"{os.getcwd()}{os.sep}data"
 csv_path = f"{os.getcwd()}{os.sep}wind_events.csv"
 
-onset_date = pd.to_datetime("2021-10-28")
-start = onset_date - pd.Timedelta(days=1)
-end = onset_date + pd.Timedelta(days=2)    # off by one, so three days are loaded now
+for d in range(14):
+    date = pd.to_datetime("2023-02-20") + pd.Timedelta(days=d)
+    logging.info(f"Analyzing flux for {date}")
+    start = date - pd.Timedelta(days=1)
+    end = date + pd.Timedelta(days=2)    # off by one, so three days are loaded now
+    try:
+        event = run_SEPevent(data_path, spacecraft_instrument="Wind 3DP", starttime=start, endtime=end, 
+                            species="e", channels=3, averaging="60min")         # maybe wget SFPD, SOPD from CDAWeb if SSL Berkeley is down
+    except ValueError:
+        continue
 
-event = run_SEPevent(data_path, spacecraft_instrument="Wind 3DP", starttime=start, endtime=end, 
-                     species="e", channels=3, averaging="15min")
+    # Data smoothing and finite difference optimization
+    data = pd.Series(event.I_data[:,3], index=event.I_times)
+    data_middle = data[date:(date + pd.Timedelta(days=1))]  # only consider the middle day for onset detection
+    data_smooth = gaussian_filter1d(data_middle, sigma=1)   
+    data_smooth_diff = np.diff(data_smooth)
+    arg_optima = []
+    for i in range(len(data_smooth_diff)-1):
+        if (data_smooth_diff[i + 1] > 0 and data_smooth_diff[i] < 0) or (data_smooth_diff[i + 1] < 0 and data_smooth_diff[i] > 0):
+            arg_optima.append(i+1)
 
-# Data smoothing and finite difference optimization
-data = pd.Series(event.I_data[:,3], index=event.I_times)
-smooth = gaussian_filter1d(data, sigma=1)
-smooth_diff = np.diff(smooth)
-arg_optima = []
-for i in range(len(smooth_diff)-1):
-    if (smooth_diff[i + 1] > 0 and smooth_diff[i] < 0) or (smooth_diff[i + 1] < 0 and smooth_diff[i] > 0):
-        arg_optima.append(i+1)
+    # Plot data, smoothed data and optima
+    fig, ax = plt.subplots()
+    ax.plot(data)
+    ax.plot(data_middle.index, data_smooth)
+    ax.plot(data_middle.index[arg_optima], data_smooth[arg_optima], "k+")
+    ax.set_yscale("log")
+    ax.set_ylabel("Intensity")
+    fig.suptitle(f"Optima for {start} - {end}")
+    fig.tight_layout()
+    plot_dir = f"{os.getcwd()}{os.sep}plots"
+    os.makedirs(plot_dir, exist_ok=True)
+    fname = plot_dir + os.sep + f"Wind_{start.strftime("%Y%m%d")}-{end.strftime("%Y%m%d")}"
+    fig.savefig(fname, bbox_inches="tight")
+    plt.close(fig)
 
-# Plot data, smoothed data and optima
-fig, ax = plt.subplots()
-ax.plot(data)
-ax.plot(data.index, smooth)
-ax.plot(data.index[arg_optima], smooth[arg_optima], "k+")
-ax.set_yscale("log")
-ax.set_ylabel("Intensity")
-fig.suptitle(f"Optima for {start} - {end}")
-fig.tight_layout()
-plot_dir = f"{os.getcwd()}{os.sep}plots"
-os.makedirs(plot_dir, exist_ok=True)
-fname = plot_dir + os.sep + f"Wind_{start.strftime("%Y%m%d")}-{end.strftime("%Y%m%d")}"
-fig.savefig(fname, bbox_inches="tight")
-
-# Rough onset determination with Poisson-CUSUM
-onset = Onset(start, end, "Wind", "3DP", "e", "l3", viewing="3", data_path=data_path)
-for i in range(len(arg_optima) - 1):
-    if (data[arg_optima].iloc[i+1] / data[arg_optima].iloc[i] > 10) and (data[arg_optima].iloc[i + 1] > data[arg_optima].iloc[i]):
-        bg_end = data[arg_optima].index[i]
-        bg_start = bg_end - pd.Timedelta(hours=8)
-        background = BootstrapWindow(bg_start, bg_end, bootstraps=1000)
-        single_onset_stats, flux_series = onset.cusum_onset(channels=[3], background_range=background, 
-                                                                    viewing="3", resample="1min", cusum_minutes=60,
-                                                                    plot=True)
-        try:
-            onset_date = single_onset_stats[-1].date()
-            onset_time = single_onset_stats[-1].time()
-        except AttributeError:
-            onset_date = ''
-            onset_time = ''
-        
-# Write to CSV
-with open(csv_path, "+r", encoding="utf-8") as fp:
-    event_no = len(fp.readlines())
-    fp.write(f"{event_no},{onset_date},{onset_time},{bg_start},{bg_end}")
+    # Rough onset determination with Poisson-CUSUM
+    onset = Onset(start, end, "Wind", "3DP", "e", "l3", viewing="3", data_path=data_path)
+    for i in range(len(arg_optima) - 1):
+        n = 0
+        opt1 = arg_optima[i]
+        opt2 = arg_optima[i+1]
+        if (data_middle.iloc[opt2] / data_middle.iloc[opt1] > 10) \
+        and (data_middle.iloc[opt2] > data_middle.iloc[opt1]) \
+        and check_for_gaps(data_middle, data_middle.index[opt1], data_middle.index[opt2]) is False:
             
+            logging.info(f"Found a potential event between {data_middle[arg_optima].index[i]} - {data_middle[arg_optima].index[i+1]}")
+            bg_end = data_middle[arg_optima].index[i]
+            bg_start = bg_end - pd.Timedelta(hours=8)
+            background = BootstrapWindow(bg_start, bg_end, bootstraps=1000)
+            single_onset_stats, flux_series = onset.cusum_onset(channels=[3], background_range=background, 
+                                                                        viewing="3", resample="1min", cusum_minutes=60,
+                                                                        plot=False)
+            try:
+                onset_date = single_onset_stats[-1].date()
+                onset_time = single_onset_stats[-1].time()
+                logging.info(f"Poisson-CUSUM result: found onset {onset_date} {onset_time}\n" \
+                             f"using background {bg_start} - {bg_end}")
+                
+                # Write to CSV
+                with open(csv_path, "+r", encoding="utf-8") as fp:
+                    event_no = len(fp.readlines())
+                    fp.write(f"{event_no},{onset_date},{onset_time},{bg_start},{bg_end}\n")
+                    logging.info(f"Results successfully saved to {csv_path}")
+                    fp.close()
+                
+                n += 1
+
+            except AttributeError:
+                logging.info(f"Sufficient minimum ({opt1}) and maximum ({opt2}) were identified, but no onset was found")
+
+    logging.info(f"Analysis for {date} finished, {n} onsets found")
